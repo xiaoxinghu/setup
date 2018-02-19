2766b051edffc6018ea7c664797cb6ecb2a39b3c
Xiaoxing Hu <dawnstar.hu@gmail.com>
2018-02-19 13:50:00 +1300

    include custom.el file

---
+++ .dist/src/.spacemacs.d/custom.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   (quote
+    ("/Users/xhu/io/desktop.org" "/Users/xhu/io/life.org" "/Users/xhu/io/posts.org" "/Users/xhu/io/projects.org" "/Users/xhu/io/notes/ai.org" "/Users/xhu/io/notes/compiler.org" "/Users/xhu/io/notes/design.org" "/Users/xhu/io/notes/emacs.org" "/Users/xhu/io/notes/finance.org" "/Users/xhu/io/notes/git.org" "/Users/xhu/io/notes/gtd.org" "/Users/xhu/io/notes/ios.org" "/Users/xhu/io/notes/misc.org" "/Users/xhu/io/notes/swift.org" "/Users/xhu/io/notes/testing.org" "/Users/xhu/io/notes/tools.org" "/Users/xhu/io/work/fiserv.org")))
+ '(package-selected-packages
+   (quote
+    (yasnippet-snippets spaceline-all-the-icons ruby-hash-syntax org-mime centered-cursor-mode zenburn-theme yapfify yaml-mode ws-butler worf winum which-key wgrep web-mode web-beautify volatile-highlights vi-tilde-fringe uuidgen use-package unfill toml-mode toc-org tagedit symon swift-mode string-inflection spaceline solaire-mode smex smeargle slim-mode scss-mode sass-mode rvm ruby-tools ruby-test-mode ruby-refactor rubocop rspec-mode robe reveal-in-osx-finder restart-emacs request rbenv rake rainbow-delimiters racer pyvenv pytest pyenv-mode py-isort pug-mode powershell popwin pippel pip-requirements persp-mode pbcopy password-generator paradox pandoc-mode ox-reveal ox-pandoc ox-hugo ox-gfm overseer osx-trash osx-dictionary orgit org-projectile org-present org-pomodoro org-journal org-download org-bullets org-brain open-junk-file neotree nameless mwim mvn move-text mmm-mode minitest meghanada maven-test-mode markdown-toc magit-gitflow macrostep lorem-ipsum livid-mode live-py-mode linum-relative link-hint less-css-mode ledger-mode launchctl json-mode js2-refactor js-doc ivy-rich ivy-purpose ivy-hydra insert-shebang info+ indent-guide importmagic impatient-mode hy-mode hungry-delete hl-todo highlight-parentheses highlight-numbers highlight-indentation hide-comnt help-fns+ helm-make groovy-mode groovy-imports graphviz-dot-mode gradle-mode google-translate golden-ratio gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe git-gutter-fringe+ gh-md fuzzy flyspell-correct-ivy flycheck-rust flycheck-pos-tip flycheck-ledger flycheck-bashate flx-ido fish-mode fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-org evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit evil-lisp-state evil-lion evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-cleverparens evil-args evil-anzu eval-sexp-fu ensime emojify emoji-cheat-sheet-plus emmet-mode elisp-slime-nav editorconfig dumb-jump doom-themes diminish diff-hl deft cython-mode csv-mode counsel-projectile counsel-css company-web company-tern company-statistics company-shell company-emoji company-emacs-eclim company-anaconda column-enforce-mode coffee-mode clean-aindent-mode chruby cargo bundler browse-at-remote auto-yasnippet auto-highlight-symbol auto-dictionary auto-compile aggressive-indent adaptive-wrap ace-window ac-ispell))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-level-1 ((t (:inherit outline-1 :height 1.0))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.0))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
