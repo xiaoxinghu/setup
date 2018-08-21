7861c5c7bbbf820f09ada05952540bf69386573c
Xiaoxing Hu <dawnstar.hu@gmail.com>
2018-08-22 10:05:47 +1200

    config

--- .dist/src/.config/fish/config.fish
+++ .dist/src/.config/fish/config.fish
@@ -5,6 +5,10 @@ set -g -x fish_greeting ''
 # use nerd font
 set -g theme_nerd_fonts yes
 # set JAVA_HOME
-# set -Ux JAVA_HOME (/usr/libexec/java_home)
+set -Ux JAVA_HOME (/usr/libexec/java_home)
 # rbenv
 # status --is-interactive; and source (rbenv init -|psub)
+set -Ux EDITOR emacs
+set -g -x ANDROID_HOME ~/Library/Android/sdk
+set -g -x PATH $ANDROID_HOME/platform-tools $PATH
+set -g -x PATH $ANDROID_HOME/build-tools/27.0.3 $PATH
